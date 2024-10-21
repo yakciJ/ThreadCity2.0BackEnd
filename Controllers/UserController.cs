55a72a2c2d525adde4e86cfeab6f0b5a8f62c361
Đỗ Đăng Huy <sieunhan482@gmail.com>
2024-10-21 17:03:28 +0700

    add userController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreadCity2._0BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
