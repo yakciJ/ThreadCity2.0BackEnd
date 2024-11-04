19d8f4a41127494b5fec08b9a180f5a21f618487
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-11-04 13:14:27 +0700

    modify CreatePostRequestDto

--- Models/DTO/Post/CreatePostRequestDto.cs
+++ Models/DTO/Post/CreatePostRequestDto.cs
@@ -4,9 +4,9 @@ namespace ThreadCity2._0BackEnd.Models.DTO.Post
 {
     public class CreatePostRequestDto
     {
-        [Required]
+        [Required(ErrorMessage = "Hãy nhập tiêu đề trước khi đăng")]
         public required string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Hãy nhập nội dung trước khi đăng")]
         public required string Content { get; set; }
     }
 }
